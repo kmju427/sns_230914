cd0a7d53b10ea264626c542def5eee46ba2e9561
DESKTOP-2HG4T8K\Kim min ju <kmju4876@gmail.com>
2024-01-17 23:06:09 +0900

    SNS - 회원가입, 로그인/로그아웃, 글 목록까지 완료

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,21 +1,34 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+
 @RequestMapping("/timeline")
 @Controller
 public class TimelineController {
 
+	@Autowired
+	private PostBO postBO;
+	
 	/**
 	 * 타임라인 화면 - http://localhost:8080/timeline/timeline-view
 	 * @param model
 	 * @return
 	 */
 	@GetMapping("/timeline-view")
-	public String listView(Model model) {
+	public String timelineView(Model model) {
+		// 게시글 목록 조회
+		List<PostEntity> postList = postBO.getPostList();
+		model.addAttribute("postList", postList);
+		
 		model.addAttribute("viewName", "timeline/timeline");
 		
 		return "template/layout";
